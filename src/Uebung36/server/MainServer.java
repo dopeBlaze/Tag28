6c4430328ed1a643e725838a51dfd18d1c43ed20
dopeBlaze <m.broja@web.de>
2020-01-22 14:42:37 +0100

    Die Klassen MainServer und Server mit Testcode geschrieben

--- src/Uebung36/server/MainServer.java
+++ src/Uebung36/server/MainServer.java
@@ -1,4 +1,29 @@
 package Uebung36.server;
 
+import Uebung36.model.VerbindungsSystem;
+import Uebung36.server.rmi.Server;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class MainServer {
+    public static void main(String[] args) throws RemoteException {
+        System.out.println("ChatServer\n----------------------------------------\n");
+
+        // Neues ServerObjekt erzeugen
+        Server server = new Server();
+
+        // Neues Objekt erstellen, das zur Registry exportiert werden soll
+        VerbindungsSystem stub = (VerbindungsSystem) UnicastRemoteObject.exportObject(server, 0);
+
+        // Standard-Port der Registry: 1099
+        Registry registry = LocateRegistry.createRegistry(VerbindungsSystem.SERVER_PORT);
+
+        // Das exportierte Objekt an die Registry binden
+        registry.rebind(VerbindungsSystem.SERVER_NAME, stub);
+
+        System.out.println("Chatserver ist gestartet und empfangsbereit.");
+    }
 }
--- src/Uebung36/server/rmi/Server.java
+++ src/Uebung36/server/rmi/Server.java
@@ -1,4 +1,17 @@
 package Uebung36.server.rmi;
 
-public class Server {
+import Uebung36.model.VerbindungsSystem;
+
+import java.rmi.RemoteException;
+
+public class Server implements VerbindungsSystem {
+    @Override
+    public void sendMessage(String message) throws RemoteException {
+
+        /*
+         * Die Message des Clients soll einfach nur ausgegeben werden.
+         * Damit es rot eingefärbt wird wird die Fehlerausgabe benutzt.
+         */
+        System.err.println(message);
+    }
 }
