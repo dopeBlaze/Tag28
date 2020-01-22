5f3a26936a56de95ee6da29e43464f401ec5aef9
dopeBlaze <m.broja@web.de>
2020-01-22 14:46:18 +0100

    Die Klassen MainClient und Client mit Testcode geschrieben

--- src/Uebung36/client/MainClient.java
+++ src/Uebung36/client/MainClient.java
@@ -1,4 +1,34 @@
 package Uebung36.client;
 
+import Uebung36.client.rmi.Client;
+import Uebung36.model.VerbindungsSystem;
+
+import java.util.Scanner;
+
 public class MainClient {
+
+    public static void main(String[] args) {
+        /* Hiermit stellen wir die Verbindung zu unserem Singleton her */
+        Client.getInstance();
+
+        // Benutzer begrüßen
+        System.out.println("Willkommen zum RMI-ChatSystem.\nZum Schreiben von Nachrichten: Einfach eine Nachricht eintippen und mit Enter bestätigen.\nZum Beenden des Programms: '" + VerbindungsSystem.BEENDEN + "' als Nachricht schreiben.");
+
+        // Von der Konsole die Nachrichten einlesen
+        Scanner scanner = new Scanner(System.in);
+
+        String message;
+
+        // So lange neue Nachrichten abfragen bis der Benutzer die Verbindung beenden will
+        do {
+            System.out.print("Client: ");
+            message = scanner.nextLine();
+
+            Client.getInstance().nachrichtSenden(message);
+        } while (!message.equals(VerbindungsSystem.BEENDEN));
+
+        // Verbindung zum Scanner und zum Server trennen
+        scanner.close();
+        Client.getInstance().disconnect();
+    }
 }
--- src/Uebung36/client/rmi/Client.java
+++ src/Uebung36/client/rmi/Client.java
@@ -1,4 +1,73 @@
 package Uebung36.client.rmi;
 
+import Uebung36.model.VerbindungsSystem;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
+
+    /* statische Referenz auf sich selbst */
+    private static Client client;
+
+    /*
+     * Hier wird die Referenz zum stub des Servers abgelegt, muss natürlich vorher
+     * vom Server an der Registry angemeldet worden sein
+     */
+    private VerbindungsSystem server;
+
+    /**
+     * Fabrikmethode
+     * @return das aktuelle RmiClient-Objekt
+     */
+    public static Client getInstance() {
+        if (client == null) {
+            client = new Client();
+        }
+
+        return client;
+    }
+
+    /**
+     * privater Konstruktor, damit keine Instanz dieser Klasse von aussen erzeugt werden kann
+     * Stellt eine Verbindung zum ChatServer her
+     */
+    private Client() {
+        try {
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1", VerbindungsSystem.SERVER_PORT);
+            server = (VerbindungsSystem) registry.lookup(VerbindungsSystem.SERVER_NAME);
+        } catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Schließt die Verbindung zum Server
+     */
+    public void disconnect() {
+        client = null;
+    }
+
+    /**
+     * Schickt die übergebene Nachricht an den ChatServer
+     *
+     * @param message zu sendende Nachricht
+     * @return true, wenn die Nachricht erfolgreich verschickt wurde, false andernfalls
+     */
+    public boolean nachrichtSenden(String message) {
+        try {
+            /*
+             * Ruft mittels des Stubs die Methode sendMessage auf dem Server auf. Wenn der
+             * Server keine Exception wirft, ist alles in Ordnung. Die Funktion nachrichtSenden
+             * gibt dann true zurück.
+             */
+            server.sendMessage(message);
+            return true;
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
